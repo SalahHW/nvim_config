d8a940ea0b5f941883ed7044d7574804670722df
Salah <salah.bouheniche@gmail.com>
2023-05-05 11:15:02 +0200

    replace space indenting with tabs indenting

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -37,7 +37,7 @@ opt.cursorline = true
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.expandtab = true        -- Use spaces instead of tabs
+opt.expandtab = false       -- Use spaces instead of tabs
 opt.shiftwidth = 4          -- Shift 4 spaces when tab
 opt.tabstop = 4             -- 1 tab == 4 spaces
 opt.smartindent = true      -- Autoindent new lines
