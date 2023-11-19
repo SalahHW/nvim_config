39773e638e0254e6cebd43e1251324e697a1a649
Salah <salah.bouheniche@gmail.com>
2023-11-19 11:00:03 +0100

    Update CMP

--- lua/settings/nvim-cmp.lua
+++ lua/settings/nvim-cmp.lua
@@ -1,32 +1,30 @@
 local cmp = require'cmp'
 
-cmp.setup({
+cmp.setup {
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+			require('luasnip').lsp_expand(args.body)
 		end,
 	},
 	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
-	mapping = cmp.mapping.preset.insert({
+	mapping = cmp.mapping.preset.insert {
 		['<C-e>'] = cmp.mapping.abort(),
 		['<C-j>'] = cmp.mapping.select_next_item(),
 		['<C-k>'] = cmp.mapping.select_prev_item(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
+		['<CR>'] = cmp.mapping.confirm { select = false }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	},
+	sources = {
+		{ name = 'path'},
 		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+		{ name = 'buffer' },
 		{ name = 'vsnip' },
-	}, {
-			{ name = 'buffer' },
-		})
-})
+	}
+}
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
