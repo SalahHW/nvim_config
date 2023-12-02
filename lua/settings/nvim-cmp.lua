1fb57589615f9489ac081b80817cd5e2e63bb1e8
Salah <salah.bouheniche@gmail.com>
2023-12-02 12:41:26 +0100

    Update

--- lua/settings/nvim-cmp.lua
+++ lua/settings/nvim-cmp.lua
@@ -17,19 +17,18 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-j>'] = cmp.mapping.select_next_item(),
+		['<C-k>'] = cmp.mapping.select_prev_item(),
+		['<C-h>'] = cmp.mapping.abort(),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		-- { name = 'nvim_lsp' },
-		-- { name = 'vsnip' }, -- For vsnip users.
-		{ name = 'luasnip' }, -- For luasnip users.
-		-- { name = 'ultisnips' }, -- For ultisnips users.
-		-- { name = 'snippy' }, -- For snippy users.
-	}, {
-			{ name = 'buffer' },
-		})
+		{ name = 'luasnip' },
+		{ name = 'nvim_lsp' },
+		{ name = 'path' },
+		{ name = 'buffer' },
+	})
 })
 
 -- Set configuration for specific filetype.
